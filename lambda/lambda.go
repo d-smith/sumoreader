c58108729ad18c41ba9dc3da3acf048b87d30ee1
d-smith <doug.smith.mail@gmail.com>
2016-08-23 07:31:39 -0700

    Updated code to read content of dropped file

--- lambda/lambda.go
+++ lambda/lambda.go
@@ -6,6 +6,10 @@ import (
 	"github.com/bitly/go-simplejson"
 	"log"
 	"os"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"io/ioutil"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 func main() {
@@ -23,15 +27,45 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for i := 0; i < len(arr); i++ {
-		s3 := records.GetIndex(i).Get("s3")
+	sess, err := session. NewSession()
+	if err != nil {
+		fmt.Println("failed to create session,", err)
+		return
+	}
 
-		key := s3.Get("object").Get("key")
-		bucket := s3.Get("bucket")
+	svc := s3.New(sess)
+
+
+	for i := 0; i < len(arr); i++ {
+		s3Rec := records.GetIndex(i).Get("s3")
+
+		key := s3Rec.Get("object").Get("key")
+		bucket := s3Rec.Get("bucket")
 		arn := bucket.Get("arn")
 		bucketName := bucket.Get("name")
 
 		fmt.Printf("process %s in bucket %s (%s)\n", key.MustString(), bucketName.MustString(), arn.MustString())
+
+		params := &s3.GetObjectInput{
+			Bucket:                     aws.String(bucketName.MustString()),
+			Key:                        aws.String(key.MustString()),
+		}
+
+		resp, err := svc.GetObject(params)
+		if err != nil {
+			fmt.Printf("Error on GetObject: %s\n", err.Error())
+			continue
+		}
+
+		if resp.Body == nil {
+			fmt.Println("Nil body - nothing to read.")
+			continue
+		}
+
+		defer resp.Body.Close()
+		bytes, err := ioutil.ReadAll(resp.Body)
+
+		fmt.Printf("Read this:\n%s\n", string(bytes))
 
 	}
 
