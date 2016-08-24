fc9944bfdc4fe12ffd7b42babb2c13cea710392f
d-smith <doug.smith.mail@gmail.com>
2016-08-24 14:12:06 -0700

    Added firehose config

---
+++ terraform/firehose/firehose.tf
@@ -0,0 +1,83 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "xtds-tf-bucket"
+  acl = "private"
+}
+
+
+resource "aws_iam_role" "firehose_api_role" {
+   name = "firehose_api_role"
+   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "firehose.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "firehose_api_policy" {
+    name = "firehose_api_policy"
+    role = "${aws_iam_role.firehose_api_role.id}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Action": [
+        "s3:AbortMultipartUpload",
+        "s3:GetBucketLocation",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:ListBucketMultipartUploads",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "arn:aws:s3:::xtds-tf-bucket",
+        "arn:aws:s3:::xtds-tf-bucket/*"
+      ]
+    },
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Action": [
+        "logs:PutLogEvents"
+      ],
+      "Resource": [
+        "arn:aws:logs:us-east-1:930295567417:log-group:/aws/kinesisfirehose/call-record-stream:log-stream:*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "call_record_stream" {
+
+  name = "call-record-stream"
+  destination = "redshift"
+
+  s3_configuration {
+    role_arn = "${aws_iam_role.firehose_api_role.arn}"
+    prefix = "cr/"
+    bucket_arn = "${aws_s3_bucket.bucket.arn}"
+    buffer_size = 10
+    buffer_interval = 60
+  }
+  redshift_configuration {
+    role_arn = "${aws_iam_role.firehose_api_role.arn}"
+    cluster_jdbcurl = "jdbc:redshift://${var.redshift_endpoint}/apidb"
+    username = "apimaster"
+    password = "${var.master_pw}"
+    data_table_name = "callrecord"
+  }
+}
---
+++ terraform/firehose/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+    description = "EC2 Region for the VPC"
+    default = "us-east-1"
+}
+
+variable "master_pw" {}
+variable "redshift_endpoint" {}
