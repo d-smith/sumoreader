409afd060ee179d44c92c3fcb71f29165df3e129
d-smith <doug.smith.mail@gmail.com>
2016-08-31 13:55:34 -0700

    Added tf to launch instance for db work in redshift

---
+++ rsmaint/instance.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["ubuntu/images/ebs/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["paravirtual"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+    ami = "${data.aws_ami.ubuntu.id}"
+    instance_type = "t1.micro"
+    tags {
+        Name = "HelloWorld"
+    }
+    key_name = "FidoKeyPair"
+    vpc_security_group_ids = ["${var.security_group}"]
+    subnet_id = "${var.subnet}"
+    user_data = "${file("dbclient_setup.txt")}"
+}
+
+output "public_dns" {
+    value = "${aws_instance.web.public_dns}"
+}
